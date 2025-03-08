16f6334d1daf76acdc1787a9da77f68bdb4c44a1
fangtao <fangtao@leoao.com>
2025-03-08 16:31:01 +0800

    Initial Commit

---
+++ Harmnoy-InstllTool/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Harmnoy-InstllTool
+//
+//  Created by 方焘 on 2025/3/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Harmnoy-InstllTool/Harmnoy_InstllToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  Harmnoy_InstllToolApp.swift
+//  Harmnoy-InstllTool
+//
+//  Created by 方焘 on 2025/3/8.
+//
+
+import SwiftUI
+
+@main
+struct Harmnoy_InstllToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
